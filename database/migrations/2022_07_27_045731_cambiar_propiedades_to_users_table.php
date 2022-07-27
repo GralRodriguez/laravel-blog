7a75995b655da95231b30ab90ad2dd30bf2054b6
Héctor Giovanni Rodríguez Ramos <gral.rodriguezr@gmail.com>
2022-07-27 00:05:06 -0500

    GralRodriguez: Wednesday 27, July 2022: Learning basic concepts of migrations and apply to database example, column properties modifications via doctrine/dbal dependency on laravel

--- database/migrations/2022_07_27_045731_cambiar_propiedades_to_users_table.php
+++ database/migrations/2022_07_27_045731_cambiar_propiedades_to_users_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('cursos', function(Blueprint $table){
-            $table->id();
-            $table->string('name');
-            $table->text('descripcion');
-            $table->timestamps(); //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', '150')->nullable()->change();
         });
     }
 
@@ -28,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', 255)->nullable(false)->change();
+        });
     }
 };
